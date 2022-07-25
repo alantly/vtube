c20dc5f388d30052ee1c2d4dedc00b8f8ef65bc6
Alan Ly <alan.ly@discordapp.com>
2022-07-25 13:40:38 -0700

    three js installed but not working yet

--- src/vtube.tsx
+++ src/vtube.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import * as THREE from 'three';
+import { GLTFLoader, GLTF } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import * as CameraUtils from '@mediapipe/camera_utils/camera_utils';
 import * as FaceMesh from '@mediapipe/face_mesh/face_mesh';
 import { drawConnectors, drawLandmarks } from '@mediapipe/drawing_utils/drawing_utils';
@@ -33,6 +35,120 @@ function onResult(canvasElement, canvasCtx) {
   }
 }
 
+
+/* function onResult(effectRenderer) {
+ *   return (results) => {
+ *     effectRenderer.render(results);
+ *   }
+ * }
+ * 
+ *  */
+class EffectRenderer {
+  VIDEO_DEPTH = 500;
+  FOV_DEGREES = 63;
+  NEAR = 1;
+  FAR = 10000;
+
+  scene: THREE.Scene;
+  renderer: THREE.WebGLRenderer;
+  faceGroup: THREE.Group;
+  canvasElement: HTMLCanvasElement;
+  camera: THREE.PerspectiveCamera;
+
+  constructor(canvasElement) {
+    this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+    this.camera.position.z = 1;
+    this.canvasElement = canvasElement;
+
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color(0xa9a9a9);
+
+    this.renderer = new THREE.WebGLRenderer({canvas: canvasElement});
+    this.renderer.outputEncoding = THREE.sRGBEncoding;
+
+    const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
+    hemiLight.position.set(0, 100, 0);
+    this.scene.add(hemiLight);
+
+    const dirLight = new THREE.DirectionalLight(0xffffff);
+    dirLight.position.set(-30, 100, -5);
+    dirLight.castShadow = true;
+    this.scene.add(dirLight);
+
+    this.faceGroup = new THREE.Group();
+    this.faceGroup.matrixAutoUpdate = false;
+    this.scene.add(this.faceGroup);
+
+    const loader = new GLTFLoader();
+    loader.setPath('https://threejs.org/examples/models/gltf/DamagedHelmet/glTF/');
+    loader.load('DamagedHelmet.gltf', (gltf) => {
+      const scene = gltf.scene;
+      scene.scale.set(18, 18, 18);
+
+      this.faceGroup.add(gltf.scene);
+    });
+  }
+
+  render(results: FaceMesh.Results) {
+    this.onCanvasDimsUpdate();
+
+    const imagePlane = this.createGpuBufferPlane(results.image);
+    this.scene.add(imagePlane);
+
+    if (results.multiFaceGeometry.length > 0) {
+      const faceGeometry = results.multiFaceGeometry[0];
+      const poseTransformMatrixData = faceGeometry.getPoseTransformMatrix();
+
+      this.faceGroup.matrix.fromArray(poseTransformMatrixData.getPackedDataList());
+      this.faceGroup.visible = true;
+    } else {
+      this.faceGroup.visible = false;
+    }
+
+    this.renderer.render(this.scene, this.camera);
+
+    this.scene.remove(imagePlane);
+  }
+
+  private createGpuBufferPlane(gpuBuffer: FaceMesh.GpuBuffer): THREE.Mesh {
+    const depth = this.VIDEO_DEPTH;
+    const fov = this.camera.fov;
+
+    const width = this.canvasElement.width;
+    const height = this.canvasElement.height;
+    const aspect = width / height;
+
+    const viewportHeightAtDepth =
+      2 * depth * Math.tan(THREE.MathUtils.degToRad(0.5 * fov));
+    const viewportWidthAtDepth = viewportHeightAtDepth * aspect;
+
+    const texture = new THREE.CanvasTexture(gpuBuffer as HTMLCanvasElement);
+    texture.minFilter = THREE.LinearFilter;
+    texture.encoding = THREE.sRGBEncoding;
+
+    const plane = new THREE.Mesh(
+        new THREE.PlaneGeometry(1, 1),
+        new THREE.MeshBasicMaterial({map: texture})
+    );
+
+    plane.scale.set(viewportWidthAtDepth, viewportHeightAtDepth, 1);
+    plane.position.set(0, 0, -depth);
+
+    return plane;
+  }
+
+  private onCanvasDimsUpdate() {
+    this.camera = new THREE.PerspectiveCamera(
+        this.FOV_DEGREES,
+        this.canvasElement.width / this.canvasElement.height,
+        this.NEAR,
+        this.FAR);
+
+    this.renderer.setSize(this.canvasElement.width, this.canvasElement.height);
+  }
+};
+
+
 const faceMesh = new FaceMesh.FaceMesh({locateFile: (file) => {
   return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh@0.4.1633559619/${file}`;
 }});
@@ -57,6 +173,8 @@ export default class App extends React.Component<{}, AppState> {
     const canvasElement = (document.getElementById('output_canvas') as HTMLCanvasElement)!;
     const canvasCtx = canvasElement.getContext('2d');
 
+    // const effectRenderer = new EffectRenderer(canvasElement);
+    // faceMesh.onResults(onResult(effectRenderer));
     faceMesh.onResults(onResult(canvasElement, canvasCtx));
 
     const camera = new CameraUtils.Camera(videoElement, {
@@ -84,4 +202,3 @@ export default class App extends React.Component<{}, AppState> {
     );
   }
 }
-
